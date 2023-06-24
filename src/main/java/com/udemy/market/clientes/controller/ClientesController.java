bcef5a22d5116b2aa5b8a5427331a39a55040d43
Juan Pablo López <juan.lopez@wizeline.com>
2023-06-24 09:00:23 -0600

    Fixes after implementing frontend code

--- src/main/java/com/udemy/market/clientes/controller/ClientesController.java
+++ src/main/java/com/udemy/market/clientes/controller/ClientesController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.udemy.market.clientes.service.ClientesService;
 
 @RestController
 @RequestMapping("market/clientes")
+@CrossOrigin("*")
 public class ClientesController {
 
     @Autowired
